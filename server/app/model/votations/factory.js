71fbf8d4a2a1a36625e7a8d28644d453389a29da
Aleksands Shaytanov <rdischain@yandex.ru>
2017-10-13 02:35:48 +0400

    add queries

--- server/app/model/votations/factory.js
+++ server/app/model/votations/factory.js
@@ -11,23 +11,41 @@ module.exports = (query, data) => {
       return 'SELECT * FROM votations ORDER BY created_at DESC;';
     case constants.GET_VOTATION_BY_ID: 
       return 'SELECT * FROM votations WHERE votations.id = ' + data.id + ';';
+    case constants.GET_FULL_VOTATION_BY_ID:
+      return 'SELECT vtn.title, vtn.description, vtn.id votationId, vtn.creator_id creatorId, '
+           + 'vtn.created_at createdAt, vt.value, u.name, u.id userId FROM '
+           + 'votations vtn LEFT JOIN votes vt ON vtn.id = vt.votation_id '
+           + 'INNER JOIN users u ON vt.creator_id = u.id '
+           + 'WHERE vtn.id = ' + data.id + ';';
     case constants.GET_ALL_LIMITED_FROM_OFFSET:
       return 'SELECT * FROM votations LIMIT ' + data.limit + ' OFFSET ' + data.offset + ';';
     case constants.GET_ALL_WITH_VOTES_LIMITED_BY_OFFSET:
-      return 'SELECT vtn.title, vtn.description, vtn.id votationId, vtn.creator_id creatorId, vt.value, u.name, u.id userId FROM '
+      return 'SELECT vtn.title, vtn.description, vtn.id votationId, vtn.creator_id creatorId, '
+           + 'vtn.created_at createdAt, vt.value, u.name, u.id userId FROM '
            + 'votations vtn LEFT JOIN votes vt ON vtn.id = vt.votation_id '
            + 'INNER JOIN users u ON vt.creator_id = u.id '
            + 'LIMIT ' + data.limit + ' OFFSET ' + data.offset + ';';
     case constants.FIND_VOTATIONS_LIMITED_FROM_OFFSET: 
       return 'SELECT * FROM votations WHERE MATCH (title,description) AGAINST (' + data.text + ') '
            + ' ORDER BY created_at DESC '
-           + 'LIMIT ' + data.limit + ' OFFSET ' + data.offset + ';';    
+           + 'LIMIT ' + data.limit + ' OFFSET ' + data.offset + ';';  
+    case constants.FIND_VOTATIONS_WITH_VOTES_LIMITED_FROM_OFFSET: {
+      console.log(data);
+      return 'SELECT vtn.title, vtn.description, vtn.id votationId, vtn.creator_id creatorId, '
+          + 'vtn.created_at createdAt, vt.value, u.name, u.id userId FROM '
+          + 'votations vtn LEFT JOIN votes vt ON vtn.id = vt.votation_id '
+          + 'INNER JOIN users u ON vt.creator_id = u.id '
+          + 'WHERE MATCH (vtn.title,vtn.description) AGAINST (' + data.text + ') '
+          + 'ORDER BY vtn.created_at DESC '
+          + 'LIMIT ' + data.limit + ' OFFSET ' + data.offset + ';'; 
+    } 
     case constants.GET_USER_VOTATIONS_LIMITED_FROM_OFFSET:
       return 'SELECT * FROM votations WHERE votations.creator_id = ' + data.creatorId
            + ' ORDER BY created_at DESC'
            + ' LIMIT ' + data.limit + ' OFFSET ' + data.offset + ';';
     case constants.GET_USER_VOTATIONS_WITH_VOTES_LIMITED_FROM_OFFSET:
-      return 'SELECT vtn.title, vtn.description, vtn.id votationId, vtn.creator_id creatorId, vt.value, u.name, u.id userId FROM '
+      return 'SELECT vtn.title, vtn.description, vtn.id votationId, vtn.creator_id creatorId, '
+           + 'vtn.created_at createdAt, vt.value, u.name, u.id userId FROM '
            + 'votations vtn LEFT JOIN votes vt ON vtn.id = vt.votation_id '
            + 'INNER JOIN users u ON vt.creator_id = u.id '
            + 'WHERE vtn.creator_id = ' + data.creatorId
