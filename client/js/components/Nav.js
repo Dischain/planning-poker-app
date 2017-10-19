ff90c132c6032d886883b889d16747f3c1a24dba
Aleksands Shaytanov <rdischain@yandex.ru>
2017-10-20 02:05:01 +0400

    add basic nav

--- client/js/components/Nav.js
+++ client/js/components/Nav.js
@@ -2,16 +2,56 @@
 
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import LoadingButton from './LoadingButton.js';
 import PropTypes from 'prop-types';
 import { logout } from '../actions/loginActions';
 
 export default class Nav extends Component {
   render() {
-    return (
-      <div className = 'nav'>
-        Navbar {this.props.loggedIn}
+    const navBtns = this.props.loggedIn ? (
+      <div>
+        <Link to='/dashboard' className='btn btn--dash btn--nav'>Dashboard</Link>
+        {this.props.sendingLogoutRequest ? (
+          <LoadingButton className='btn--nav' btnText = {'Login'}/>
+        ) : (
+          <a href='#' 
+            className='btn btn--login btn--nav' 
+            onClick={this._logout.bind(this)}>Logout
+          </a>
+        )}
+      </div>
+    ) : (
+      <div>
+        <Link to='/register' className='btn btn--login btn--nav'>Register</Link>
+        <Link to='/login' className='btn btn--login btn--nav'>Login</Link>
       </div>
     );
+    return (
+      <div className = 'nav'>
+        <div className='nav__wrapper'>
+          <Link to='/' className='nav__logo-wrapper'><h1 className='nav__logo'>Login&nbsp;Flow</h1></Link>
+          { navBtns }
+        </div>
+      </div>
+    );
+  }
+
+  _logout() {
+    this.props.logout();
+  }
+}
+
+function mapStateToProps({ loginReducer }) {
+  return {
+    sendingLogoutRequest: loginReducer.sendingLogoutRequest,
+    loggedIn: loginReducer.loggedIn
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch: func => dispatch(func),
+    logout: userData => dispatch(logout())
   }
 }
 
