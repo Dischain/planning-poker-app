e95cb4799b93927a858af69a5cb5bac992b068b0
Aleksands Shaytanov <rdischain@yandex.ru>
2017-11-14 23:44:06 +0400

    get rid of search actions

--- client/js/reducers/index.js
+++ client/js/reducers/index.js
@@ -4,7 +4,6 @@ import { combineReducers } from "redux";
 import userReducer from './userReducer.js';
 import loginReducer from './loginReducer.js';
 import registerReducer from './registerReducer.js';
-import searchReducer from './searchReducer.js';
 import dataListReducer from './datalistReducer.js';
 import votationsReducer from './votationsReducer.js';
 
@@ -12,7 +11,6 @@ export default combineReducers({
   userReducer,
   loginReducer,
   registerReducer,
-  searchReducer,
   dataListReducer,
   votationsReducer
 });
