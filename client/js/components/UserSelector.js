eb4e54e7435a92d28a1bb7ac354aba6edfe2c041
Aleksands Shaytanov <rdischain@yandex.ru>
2017-11-17 01:34:55 +0400

    initial user selector

---
+++ client/js/components/UserSelector.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class UserSelector extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
